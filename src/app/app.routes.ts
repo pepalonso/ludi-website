6fb7dc6244d408766edbfb9bb9e51d3a9f637416
Pep Alonso <pepalonsocosta@gmail.com>
2024-10-23 00:06:34 +0200

    feat(main): agefir ruta per /ludi3x3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redirigeix per defecte a /ludi3x3
+  { path: '', redirectTo: '/ludi3x3', pathMatch: 'full' },
+
+  // ruta per les incripcions del 3x3
+  { path: 'ludi3x3', component: AppComponent },
+];
