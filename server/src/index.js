e4cb104f23bc2e4f673f44270133a65703a66c44
Pep Alonso <pepalonsocosta@gmail.com>
2024-10-27 15:57:14 +0100

    feat(develop): add lint to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,8 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import {
-  DynamoDBDocumentClient,
-  PutCommand,
-  ScanCommand,
-} from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { v4 as uuid } from "uuid";
 import { getDynamoDbConfig, getTableName } from "./config.js";
-import { getDate } from "./utils.js";
+import { checkExistingFields, getDate } from "./utils.js";
 
 export const handler = async (event) => {
   const dynamoDbConfig = getDynamoDbConfig();
@@ -29,33 +25,31 @@ export const handler = async (event) => {
 
   const new_id = uuid();
   const date = getDate();
-  console.log("date", date);
-  const team_name = body.NOM_EQUIP;
-
-  const scan_params = {
-    TableName: table,
-    FilterExpression:
-      "NOM_EQUIP = :teamName OR NUMERO_CONTACTE = :contactNumber OR MAIL_CONTACTE = :email",
-    ExpressionAttributeValues: {
-      ":teamName": team_name,
-      ":contactNumber": body.NUMERO_CONTACTE,
-      ":email": body.MAIL_CONTACTE,
-    },
-  };
+  const nom_equip = body.NOM_EQUIP;
+  const numero_contacte = body.NUMERO_CONTACTE;
+  const mail_contacte = body.MAIL_CONTACTE;
+  const jugadors = body.JUGADORS;
 
   try {
-    const data = await ddbDocClient.send(new ScanCommand(scan_params));
-    if (data.Items && data.Items.length > 0) {
+    const conflicts = await checkExistingFields(
+      ddbDocClient,
+      nom_equip,
+      numero_contacte,
+      mail_contacte,
+    );
+    if (conflicts.length > 0) {
       return {
         statusCode: 400,
-        body: JSON.stringify({ message: "Nom del Equip ja esta agafat" }),
+        body: JSON.stringify({
+          message: `Conflicte: ${conflicts.join(", ")} ja esta agafat!`,
+        }),
       };
     }
   } catch (error) {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        message: "Error al fer la query en la database",
+        message: "Error fent scan a la taula",
         error: error.message,
       }),
     };
@@ -65,43 +59,32 @@ export const handler = async (event) => {
     TableName: table,
     Item: {
       ID_EQUIP: new_id,
-      NOM_EQUIP: team_name,
+      NOM_EQUIP: nom_equip,
       DATA_INCRIPCIO: date,
 
-      NOM_JUGADOR_1: body.NOM_JUGADOR_1,
-      NOM_JUGADOR_2: body.NOM_JUGADOR_2,
-      NOM_JUGADOR_3: body.NOM_JUGADOR_3,
-      ...(body.NOM_JUGADOR_4 && { NOM_JUGADOR_4: body.NOM_JUGADOR_4 }),
-      ...(body.NOM_JUGADOR_5 && { NOM_JUGADOR_5: body.NOM_JUGADOR_5 }),
+      JUGADORS: jugadors.map((jugador) => ({
+        NOM: jugador.NOM,
+        NEIXAMENT: jugador.NEIXAMENT,
+        TALLA_SAMARRETA: jugador.TALLA_SAMARRETA,
+      })),
 
-      TALLA_JUGADOR_1: body.TALLA_JUGADOR_1,
-      TALLA_JUGADOR_2: body.TALLA_JUGADOR_2,
-      TALLA_JUGADOR_3: body.TALLA_JUGADOR_3,
-      ...(body.TALLA_JUGADOR_4 && { TALLA_JUGADOR_4: body.TALLA_JUGADOR_4 }),
-      ...(body.TALLA_JUGADOR_5 && { TALLA_JUGADOR_5: body.TALLA_JUGADOR_5 }),
-
-      DATA_JUGADOR_1: body.DATA_JUGADOR_1,
-      DATA_JUGADOR_2: body.DATA_JUGADOR_2,
-      DATA_JUGADOR_3: body.DATA_JUGADOR_3,
-      ...(body.DATA_JUGADOR_4 && { DATA_JUGADOR_4: body.DATA_JUGADOR_4 }),
-      ...(body.DATA_JUGADOR_5 && { DATA_JUGADOR_5: body.DATA_JUGADOR_5 }),
-
-      NUMERO_CONTACTE: body.NUMERO_CONTACTE,
-      MAIL_CONTACTE: body.MAIL_CONTACTE,
+      NUMERO_CONTACTE: numero_contacte,
+      MAIL_CONTACTE: mail_contacte,
     },
   };
   console.log("params", { params });
 
   try {
     const data = await ddbDocClient.send(new PutCommand(params));
-    console.log("Success - item added or updated", data);
+    console.log("Success - item added or updated");
 
     const response = {
       statusCode: 200,
       body: JSON.stringify({
         message: "Equip afegit correctament",
-        data,
         id_equip: new_id,
+        hora: date,
+        data,
       }),
     };
     return response;
