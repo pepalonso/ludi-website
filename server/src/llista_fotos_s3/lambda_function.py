1b89a0b2d10783775fb41504c4434b7ba6517092
Pep Alonso <pepalonsocosta@gmail.com>
2025-05-28 20:13:14 +0200

    correct lambda

--- server/src/llista_fotos_s3/lambda_function.py
+++ server/src/llista_fotos_s3/lambda_function.py
@@ -1,33 +1,56 @@
 import boto3
 import os
+import json
 
-s3 = boto3.client('s3')
+# Configure S3 client (no endpoint_url if using AWS S3!)
+s3 = boto3.client(
+    's3',
+    region_name=os.getenv('AWS_REGION'),
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID_M'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY_M')
+)
 
 def lambda_handler(event, context):
-    BUCKET = os.environ.get('galeria-web')
+    BUCKET = os.getenv('BUCKET')
 
     year = event.get('queryStringParameters', {}).get('year', 'none')
     PREFIX = f"{year}/"
 
     try:
         response = s3.list_objects_v2(Bucket=BUCKET, Prefix=PREFIX)
-
         contents = response.get('Contents', [])
+
+        # Construct URLs using S3 regional endpoint
+        region = os.getenv('AWS_REGION')
         urls = [
-            f"https://{BUCKET}.s3.amazonaws.com/{obj['Key']}"
-            for obj in contents if not obj['Key'].endswith('/')
-        ]
+    s3.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': BUCKET, 'Key': obj['Key']},
+        ExpiresIn=3600  # valid for 1 hour
+    )
+    for obj in contents if not obj['Key'].endswith('/')
+]
+
 
         return {
             'statusCode': 200,
             'headers': {
-                'Access-Control-Allow-Origin': '*'
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET'
             },
-            'body': str(urls)
+            'body': json.dumps({'urls': urls})
         }
 
     except Exception as e:
         return {
             'statusCode': 500,
-            'body': str(e)
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET'
+            },
+            'body': json.dumps({'error': str(e)})
         }
