7a7efd5f6b6d6acc1d9ec3fda4adeef1c6835686
Pep Alonso <palonso@hubtype.com>
2026-03-10 08:08:17 +0100

    fix contact number on error modal

--- client/src/app/utils/redirector/redirector.component.ts
+++ client/src/app/utils/redirector/redirector.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-redirector',
@@ -11,6 +12,7 @@ export class RedirectorComponent {
   constructor(private router: Router) {}
 
   ngOnInit() {
-    window.location.href = 'https://wa.me/659173158';
+    const phone = (environment.contactPhone || '').replace(/\D/g, '');
+    window.location.href = `https://wa.me/${phone || '659173158'}`;
   }
 }
